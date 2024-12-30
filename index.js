b10845784b439264de35b2d7d49b30969152b172
Anit George <anitsamraju@gmail.com>
2024-12-30 17:18:03 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+
+var randomNumber1 =Math.floor(Math.random() *  6) + 1;
+
+var randomDiceImgSrc ="images/dice" + randomNumber1 + ".png";
+document.querySelector(".img1").setAttribute("src",randomDiceImgSrc);
+
+var randomNumber2 =Math.floor(Math.random() *  6) + 1;
+var randomDiceImgSrc2 ="images/dice" + randomNumber2 + ".png";
+document.querySelector(".img2").setAttribute("src",randomDiceImgSrc2);
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML ="⛳Player 1 Wins!"
+}
+
+else if (randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML =" ⛳Player 2 Wins!"
+}
+else{
+     document.querySelector("h1").innerHTML =" 🤝Draw!!"
+}
